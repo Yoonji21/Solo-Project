0ee7f72e0853ba948ee3d144969c2c41caeb46b3
Yoonji21 <dayen.com.nate@gmail.com>
2024-05-15 23:46:12 +0900

    수정함

--- Assets/02. Scripts/Test.cs
+++ Assets/02. Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.DrawRay(transform.position, new Vector3(10, 0 ,0 ), new Color(0, 1, 0), LayerMask.NameToLayer("Ray"));
+        Debug.DrawRay(transform.position, new Vector3(10, 0 ,0 ), new Color(0, 1, 0), LayerMask.GetMask("Ray"));
 
         RaycastHit2D ray = Physics2D.Raycast(transform.position, new Vector3(10, 0, 0), LayerMask.NameToLayer("Ray"));
 
